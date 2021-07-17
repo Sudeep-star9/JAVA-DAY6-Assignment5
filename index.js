ec142f434415df6284e8f4a9c06cb6e3a453d25b
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:23:58 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let Alpha=['a','b','c','d','e']
+var i=0;
+do{
+console.log(Alpha)
+i++
+}
+while(i<Alpha)
+
+
+//using in object
+let Letters={
+    a:'b',
+    c:'d',
+    e:'f',
+}
+do{
+    console.log(Letters)
+}
+while(i>Letters)
